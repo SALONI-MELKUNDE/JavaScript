8353896f1fd1d290fde4f08e1a84e83919329052
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2024-10-28 21:27:02 +0100

    added some new exercies

---
+++ circle.js
@@ -0,0 +1,13 @@
+const circle = {
+    redius: 1,
+    location: {
+        x: 1,
+        y: 2
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw();
---
+++ constructorproperty.js
@@ -0,0 +1,21 @@
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('draw')
+        }
+    };
+}
+
+const circle = createCircle(1);
+
+//construction function
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const another = new Circle(1);
